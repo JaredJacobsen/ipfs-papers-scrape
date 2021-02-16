30172129c9872d8f4e7caf374b5c8898c31ee7b5
JaredJacobsen <jaredtjacobsen@gmail.com>
2021-02-16 18:54:28 +0700

    Move file

--- src/functions/fetch/fetch-pdf.js
+++ src/functions/fetch/fetch-pdf.js
@@ -1,5 +1,5 @@
 import fetchPdfFromUrl from "./fetch-pdf-from-url";
-import fetchPdfFromScihub from "../pdf/fetchPdfFromScihub";
+import fetchPdfFromScihub from "./fetch-pdf-from-scihub";
 
 export default async function fetchPdf(doi, url_for_pdf) {
   return url_for_pdf ? fetchPdfFromUrl(url_for_pdf) : fetchPdfFromScihub(doi);
